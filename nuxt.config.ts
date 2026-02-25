a9fea5e6177e62df5a0f7f5c5302ad72791fa1c6
AllanBosire <PrimalDjinn>
2026-02-25 11:35:20 +0300

    updated deps

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,18 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	devtools: { enabled: true },
-	modules: ["@nuxt/ui"],
-	future: {
-		compatibilityVersion: 4,
-	},
-	extends: ["github:kgarchie/nuxt-starter#1"],
-	compatibilityDate: "2025-02-04",
-	nitro: {
-		imports: {
-			dirs: ["./shared/utils", "./shared/types"],
-		},
-	},
-	imports: {
-		dirs: ["../shared/types", "../shared/utils"],
-	},
+  devtools: { enabled: true },
+  modules: ["@nuxt/ui"],
+  future: {
+    compatibilityVersion: 4,
+  },
+  extends: ["github:kgarchie/nuxt-starter"],
+  compatibilityDate: "2025-02-04",
+  nitro: {
+    imports: {
+      dirs: ["./shared/utils", "./shared/types"],
+    },
+  },
+  imports: {
+    dirs: ["../shared/types", "../shared/utils"],
+  },
+  telemetry: {
+	enabled: false
+  }
 });
